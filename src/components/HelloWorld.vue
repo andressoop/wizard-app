d954b0c13c86f7f21b925b84a6581c73d9f6810f
Andre Pukk <andrep615@hotmail.com>
2019-02-01 16:41:15 +0200

    Connect to Firebase Database

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,38 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="test in tests" v-bind:key="test.id">
+       {{test.id}} | {{test.test_id}} | {{test.test_description}}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import db from './firebaseInit'
+
 export default {
   name: 'HelloWorld',
+  data () {
+    return {
+      tests: []
+    }
+  },
+  created () {
+    db.collection('test').get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        // console.log(doc.data());
+        const data = {
+          'id': doc.id,
+          'test_id': doc.data().test_id,
+          'test_description': doc.data().test_description
+        }
+        this.tests.push(data)
+      })
+    })
+  },
   props: {
     msg: String
   }
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyA2u-JcH_bFcXkvDq4eruRctwvV4nrbiO0",
+    authDomain: "wizard-app-678c1.firebaseapp.com",
+    databaseURL: "https://wizard-app-678c1.firebaseio.com",
+    projectId: "wizard-app-678c1",
+    storageBucket: "wizard-app-678c1.appspot.com",
+    messagingSenderId: "966026594810"
+}
